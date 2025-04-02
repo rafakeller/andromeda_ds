9ad9260485aaa351ab2393c509c09db425150b89
Rafaela.Santos <rafaela.santos@caixavidaeprevidencia.com.br>
2025-04-02 13:31:49 -0300

    Primeiro commit:Configurando ambiente storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    '../docs/stories/**/*.mdx',
+    '../docs/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+export default config;
